4725926c0252d97d247eaa3c3a42544647223606
margongora <margongora0803@gmail.com>
2024-05-11 00:23:17 -0500

    Added external links to Github for projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,26 @@
+import { Link } from "@phosphor-icons/react";
+
 interface props {
   children?: React.ReactNode
   icon: string,
   title: string,
-  desc: string
+  desc: string,
+  link?: string
 }
 
-const Project = ({ children, icon, title, desc }: props) => {
+const Project = ({ children, icon, title, desc, link }: props) => {
   return (
-    <div className='m-2 p-2 flex flex-col text-center items-center bg-white rounded-2xl text-black gap-2'>
-      <span
-        className='font-bold m-4 h-[20%]'
-      >{title}</span>
+    <div className='m-2 p-2 flex flex-col text-center items-center bg-white rounded-2xl text-black'>
+      <div className="flex flex-row m-2 gap-2">
+        <span
+          className='font-bold h-[20%]'
+        >
+          {title}
+        </span>
+        <a className={`hover:cursor-pointer ${link ? null : 'hidden'}`} href={link} >
+          <Link />
+        </a>
+      </div>
       <p>{desc}</p>
       <img
         className='m-4 border-4 border-black rounded-full'
--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -22,24 +22,40 @@ export default function Portfolio() {
     <>
       <Card>
         <div className='m-4 p-4 grid grid-cols-3 gap-4'>
-          <Project icon={Neovim} title='Neovim Configuration' desc='My personal Neovim configuration.'>
+          <Project
+            icon={Neovim}
+            title='Neovim Configuration'
+            desc='My personal Neovim configuration.'
+            link='https://github.com/margongora/dotfiles'
+          >
             <LuaPlain size={36} />
           </Project>
-          <Project icon={Website} title='Personal Website' desc='My personal portfolio and website.'>
+          <Project
+            icon={Website}
+            title='Personal Website'
+            desc='My personal portfolio and website.'
+            link='https://github.com/margongora/personal-website'
+          >
             <TypescriptPlain size={36} />
             <ReactOriginal size={36} />
             <TailwindcssOriginal size={36} />
             <VitejsOriginal size={36} />
           </Project>
-          <Project icon={Record} title='Record Records'
+          <Project
+            icon={Record}
+            title='Record Records'
             desc='CSCE 310 Group Project: Ayo Fatoye, Carson Lee, Justin Abraham'
+            link='https://github.com/Summer23-CSCE-310-Database-Systems/github-setup-recordrecords'
           >
             <PythonOriginal size={36} />
             <PostgresqlOriginal size={36} />
             <FlaskOriginal size={36} />
           </Project>
-          <Project icon={Revs} title={`Rev's POS System`}
+          <Project
+            icon={Revs}
+            title={`Rev's POS System`}
             desc='CSCE 331 Final Project: Adam Teo, Aaron Matthews, Akshay Belle, Chris Avila, Gabe Floreslovo'
+            link='https://github.com/csce-315-331-2024a/project-3-full-stack-agile-web-project-3-907-30'
           >
             <TypescriptPlain size={36} />
             <ReactOriginal size={36} />
