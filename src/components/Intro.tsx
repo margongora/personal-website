2c22f148174c9c0110447414efbc61fadc976878
margongora <margongora0803@gmail.com>
2024-12-25 17:25:11 -0600

    Rewrote intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
       <div className='basis-3/4 flex flex-col p-8 bg-white rounded-2xl content-center text-center text-black gap-1'>
         <span className='text-2xl font-semibold underline mb-2'>About Me!</span>
         <p className='text-lg'>
-          Howdy! I am currently a Computer Engineering student, as well as a Peer Teacher, at Texas A&M University.
+          Howdy! I am currently a senior Computer Engineering student, a Teaching Assistant for CSCE 121, and a Peer Teacher for ENGR 102 and 217 at Texas A&M University.
         </p>
         <p className='text-lg'>
           I've garnered a great deal of love and respect for the field I am trying to enter, from learning the intricacies of digital design, circuits, and computer architecture to system design, data structures, and databases.
@@ -14,7 +14,7 @@ const Intro = () => {
           I’m very quick to learn new things and new technologies, both as required of me and on my own time.
         </p>
         <p className='text-lg'>
-          My time as a former Teaching Assistant and current Peer Teacher has only fostered my love of learning, and ensured that I want to actively learn new things, not only for my career, but for the sake of learning and sharing that knowledge with others. I hope to pass this onto others, and give back to people the same way that I’ve been given my opportunities and passions.
+          My time as a Teaching Assistant and Peer Teacher has only fostered my love of learning, and ensured that I want to actively learn new things, not only for my career, but for the sake of learning and sharing that knowledge with others. I hope to pass this onto others, and give back to people the same way that I’ve been given my opportunities and passions.
           I’ll be using this website as a way for prospective employers to get to know about me some more. My qualifications and resume will be hosted here, as well as any former and current projects that I’m working on, whether they be personal, or school related.
         </p>
       </div>
